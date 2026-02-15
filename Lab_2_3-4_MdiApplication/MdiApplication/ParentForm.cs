151b98ab2fbfcac36c64671b96b1cbe9ecfd999c
AlwaysKeito <rospikachu@gmail.com>
2026-02-16 00:41:43 +0300

    ЛабораторнаяРабота2_Упражнения3-4

---
+++ Lab_2_3-4_MdiApplication/MdiApplication/ParentForm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MdiApplication
+{
+    public partial class ParentForm : Form
+    {
+        private int openDocuments = 0;
+
+        public ParentForm()
+        {
+            InitializeComponent();
+            spData.Text = DateTime.Today.ToLongDateString();
+        }
+
+        private void NewMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm newChild = new ChildForm();
+            newChild.MdiParent = this;
+
+            newChild.Text = newChild.Text + " " + ++openDocuments;
+
+            newChild.Show();
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void WindowCascadeMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+        }
+
+        private void WindowTileMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.Tag.ToString())
+            {
+                case "NewDoc":
+                 
+                    break;
+
+                case "Cascade":
+                    this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+                    spWin.Text = "Windows is cascade"; 
+                    break;
+
+                case "Title":
+                    this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+                    spWin.Text = "Windows is horizontal"; 
+                    break;
+            }
+        }
+
+        private void ParentForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
